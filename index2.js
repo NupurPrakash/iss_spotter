7181a8edc095f980b12486026be496e4aa08ed31
NupurPrakash <prakashnupurprakash@gmail.com>
2020-06-28 12:49:42 +0000

    implemented printpasstimes function

---
+++ index2.js
@@ -0,0 +1,25 @@
+//const { fetchMyIP } = require('./iss_promised');
+//const { fetchCoordsByIP } = require('./iss_promised');
+//const { fetchISSFlyOverTimes } = require('./iss_promised');
+const { nextISSTimesForMyLocation } = require('./iss_promised');
+/*
+fetchMyIP()
+    .then(fetchCoordsByIP)
+    .then(fetchISSFlyOverTimes)
+    .then(body => console.log(body));
+
+*/
+const printPassTimes = function(passTimes) {
+
+  for (const pass of passTimes) {
+    const datetime = new Date(0);
+    datetime.setUTCSeconds(pass.risetime);
+    const duration = pass.duration;
+    console.log(`Next pass at ${datetime} for ${duration} seconds`);
+  }
+};
+
+nextISSTimesForMyLocation()
+  .then((passTimes) => {
+    printPassTimes(passTimes);
+  });
