ebdc269c062deacd43d5a8934f72a88b31eca193
NupurPrakash <prakashnupurprakash@gmail.com>
2020-06-28 12:48:52 +0000

    Implemented promises

---
+++ iss_promised.js
@@ -0,0 +1,31 @@
+const request = require('request-promise-native');
+
+const fetchMyIP = function() {
+  return request('https://api.ipify.org?format=json');
+};
+const fetchCoordsByIP = function(body) {
+  const ip = JSON.parse(body).ip;
+  return request(`https://ipvigilante.com/json/${ip}`);
+
+};
+const fetchISSFlyOverTimes = function(body) {
+  const { latitude, longitude } = JSON.parse(body).data;
+  return request(`http://api.open-notify.org/iss-pass.json?lat=${latitude}&lon=${longitude}`);
+};
+const nextISSTimesForMyLocation = function() {
+  return fetchMyIP()
+    .then(fetchCoordsByIP)
+    .then(fetchISSFlyOverTimes)
+    .then((data) => {
+      const { response } = JSON.parse(data);
+      return response;
+
+    })
+    .catch((error) => {
+      console.log("Didnt work:". error.message);
+    });
+};
+
+
+ 
+module.exports = { nextISSTimesForMyLocation };
